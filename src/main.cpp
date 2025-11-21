b9e5c0e6af8c16687f7481c7e9bb25c676982e09
avkumaran <149909750+avkumaran@users.noreply.github.com>
2025-11-20 22:52:02 -0500

    progress from Avi and Nathanael on Thursday night

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,27 @@
 #include <MpuControl.hpp>
 #include <SpeakerControl.hpp>
 
+#include "LittleFS.h"
+
 BmpControl bmpSensor;
 MpuControl mpuSensor;
 SpeakerControl speaker;
+SensorData currData;
+FsWriter writer;
 
 void setup() {
     Serial.begin(115200);
     delay(3000);
+    if (!LittleFS.exists("/flight.csv")) {
+        Serial.println(F("NO_FILE_AFTER_HEADER_WRITE"));
+    }
 
     // Establish connection with sensors
     bmpSensor.connectBmp();
     mpuSensor.connectMpu();
 
+    writer.initiate(); // loops forever if file is already there
+    
     // Wait for rocket to be turned upside down for five seconds
     // before starting logging.
     while(!mpuSensor.checkUpsideDown())
@@ -27,7 +36,7 @@ void setup() {
     }
     
     // Wait for 5 mins after upside-down event
-    for(int i = 0; i < 3000; i++)
+    for(int i = 0; i < 30; i++)
     {
         // Beep fervantly
         speaker.beep(0.1);
@@ -39,18 +48,16 @@ void setup() {
 }
 
 void loop() {
-    SensorData currData;
-    FsWriter writer;
+
 
     bmpSensor.pollBmp(currData);
     mpuSensor.pollMpu(currData);
     currData.t_ms = millis();
     writer.store(currData);
 
-    writer.flush();
-
     if(BOOTSEL || (millis() > (1000*60)))
     {
+        speaker.silence();
         writer.streamFSData();
         for(;;);
     }
