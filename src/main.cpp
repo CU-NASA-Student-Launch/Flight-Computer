24715e5432a4e711af3d3fe4ef6bb4552d40f96f
nathanaelluke <nathanael.jack.24@gmail.com>
2026-01-13 14:06:10 -0500

    init commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,69 @@
-#include <Arduino.h>
-#include <FsWriter.hpp>
-#include <SensorData.hpp>
-#include <BmpControl.hpp>
-#include <MpuControl.hpp>
-#include <SpeakerControl.hpp>
+// SPDX-FileCopyrightText: 2023 Carter Nelson for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+// --------------------------------------
+// i2c_scanner
+//
+// Modified from https://playground.arduino.cc/Main/I2cScanner/
+// --------------------------------------
 
-#include "LittleFS.h"
+#include <Wire.h>
 
-BmpControl bmpSensor;
-MpuControl mpuSensor;
-SpeakerControl speaker;
-SensorData currData;
-FsWriter writer;
-
-int logStart = 0;
+// Set I2C bus to use: Wire, Wire1, etc.
+#define WIRE Wire
 
 void setup() {
-    Serial.begin(115200);
-    delay(3000);
+  WIRE.begin();
 
-    // Establish connection with sensors
-    bmpSensor.connectBmp();
-    mpuSensor.connectMpu();
-
-    writer.initiate(); // loops forever if file is already there
-    
-    // Wait for rocket to be turned upside down for five seconds
-    // before starting logging.
-    while(!mpuSensor.checkUpsideDown())
-    {
-        speaker.beep(1);
-        delay(1000);
-        speaker.beep(1);
-    }
-    
-    // Wait for 5 mins after upside-down event
-    for(int i = 0; i < 15; i++)
-    {
-        // Beep fervantly
-        speaker.beep(0.1);
-        delay(100);
-    }
-
-    speaker.blare();
-    while(!mpuSensor.checkMotion());
-    logStart = millis();
+  Serial.begin(9600);
+  while (!Serial)
+     delay(10);
+  Serial.println("\nI2C Scanner");
 }
 
+int counter = 0;
+
 void loop() {
+  byte error, address;
+  int nDevices;
 
+  Serial.println("Scanning...");
 
-    bmpSensor.pollBmp(currData);
-    mpuSensor.pollMpu(currData);
-    currData.t_ms = millis();
-    writer.store(currData);
+  nDevices = 0;
+  for(address = 1; address < 127; address++ )
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    WIRE.beginTransmission(address);
+    error = WIRE.endTransmission();
 
-    constexpr int tenMinMilli = 1000*60*10; // Ten minutes in milliseconds
-
-    if(BOOTSEL || (millis() > (tenMinMilli + logStart)))
+    if (error == 0)
     {
-        speaker.silence();
-        writer.streamFSData();
-    }
+      Serial.print("I2C device found at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
 
-    speaker.beep(0.031);
+      nDevices++;
+    }
+    else if (error==4)
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }
+  }
+
+  Serial.println(counter);
+  counter++;
+
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+
+  delay(5000);           // wait 5 seconds for next scan
 }
